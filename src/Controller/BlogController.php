2518f7ce585869c6721f82c07e0cb3bbc6cd2e38
imdad <i.t@aa>
2022-05-20 10:02:06 +0200

    Ajout page modification d'un article

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -144,4 +144,44 @@ class BlogController extends AbstractController
         return $this->redirectToRoute('blog_publication_list');
 
     }
+
+
+    /**
+     * Contrôleur de la page permettant de modifier un article existant via son id passé dans l'url
+     */
+    #[Route('/publications/modifier/{id}/', name: 'publication_edit', priority: 10)]
+    #[IsGranted('ROLE_ADMIN')]
+    public function publicationEdit(Article $article, Request $request, ManagerRegistry $doctrine, SluggerInterface $slugger): Response
+    {
+
+            // Instanciation d'un nouveau formulaire basé sur $article qui contient déjà les données actuelles de l'article à modifier
+            $form =$this->createForm(NewArticleFormType::class, $article);
+
+            $form->handleRequest($request);
+
+            // Si le formulaire est envoyé et sans erreur
+            if($form->isSubmitted() && $form->isValid()){
+
+                // Sauvegarde des données modifiées en BDD
+                $article->setSlug( $slugger->slug($article->getTitle() )->lower());
+                $em = $doctrine->getManager();
+                $em->flush();
+
+                // Message flash de succès
+                $this->addFlash('success', 'Publication modifiée avec succès !');
+
+                // Redirection vers l'article modifié
+                return $this->redirectToRoute('blog_publication_view', [
+                    'id' => $article->getId(),
+                    'slug' => $article->getSlug(),
+                    ]);
+
+            }
+
+            return $this->render('blog/publication_edit.html.twig', [
+                'form' => $form->createView(),
+            ]);
+
+        }
+
 }
